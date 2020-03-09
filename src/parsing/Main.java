1d2e6d0c5ad7649aace6dbe9dbbf24c877ca92f2
sembarner22 <ildar1001@mail.ru>
2020-03-09 14:19:00 +0300

    Tests in artifacts, it's content should be put in out/production/GenericsTabulator

--- src/parsing/Main.java
+++ src/parsing/Main.java
@@ -2,12 +2,7 @@ package parsing;
 
 import exceptions.EvaluatingException;
 import exceptions.ParsingException;
-import operator.BigIntegerOperator;
-import operator.DoubleOperator;
 import operator.FloatOperator;
-import operator.IntegerOperator;
-
-import java.math.BigInteger;
 
 public class Main extends ExpressionParser {
     public static void main(String args[]) throws ParsingException, EvaluatingException {
